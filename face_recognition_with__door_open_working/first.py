3a641d605bdafe11bc55af5236794fff89702bb0
ertugrulyilmaz99 <47754445+ertugrulyilmaz99@users.noreply.github.com>
2019-06-28 09:51:49 +0300

    Add files via upload

---
+++ face_recognition_with__door_open_working/first.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter.filedialog import *
+from face_recognition_casper import CasperFaceID
+
+class main:
+
+    def start(argv):
+        nRow = None
+        nCol = None
+
+        userGui = tk.Tk()
+        xSize, ySize = 300, 300
+        size = str(ySize) + "x" + str(xSize)
+        userGui.geometry(size)
+        userGui.title("PASS - LOCK")
+        userGui.configure(bg="deep sky blue")
+
+
+        for r in range(2):
+            for c in range(2):
+                if r == 0:
+                    Label(userGui, bg='red').grid(row=r, column=c, padx=(xSize / 6) - 15, pady=20)
+                else:
+                    Label(userGui, bg='blue', text="test").grid(row=r, column=c, padx=0, pady=(ySize /2))
+
+        addPersonButton = Button(userGui, text='Add Person', command=lambda :CasperFaceID().addPerson(), borderwidth=1, relief=RAISED)
+        addPersonButton.grid(row=1, column=0, sticky='', padx=20, pady=0)
+
+        detectPersonButton = Button(userGui,text="Detect Person", command = lambda : CasperFaceID().detect())
+        detectPersonButton.grid(row=1, column=1, sticky='', padx=20, pady=20)
+        userGui.mainloop()
+
+
+if __name__ == "__main__":
+   main.start(sys.argv[1:])
---
+++ face_recognition_with__door_open_working/gui.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from face_recognition_casper import CasperFaceID
+#from tkinter.filedialog import *
+import sys
+
+class user:
+    def pir(self):
+        print("a")
+
+    def start(argv):
+        userGui  = tk.Tk()
+
+        userGui.title("Casper Face Recognition")
+
+        tk.Label(userGui,text="Lock - Pass").pack()
+        tk.Button(userGui,text="Add Person", command=lambda: CasperFaceID().addPerson()).pack()
+        tk.Button(userGui, text="Detect Person", command=lambda: CasperFaceID().detect()).pack()
+
+        userGui.mainloop()
+
+if __name__ == "__main__":
+   user.start(sys.argv[1:])
+
+
